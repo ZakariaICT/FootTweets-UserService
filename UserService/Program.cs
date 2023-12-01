22fd09dd12e41c47edafa031b657d33f598ce52e
Hassan,Zakaria <z.hassan@student.fontys.nl>
2023-12-01 15:52:39 +0100

    x

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>
+builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
 
 builder.Services.AddControllers();
 
