b3ab7c44fb7fd9a614f228a11b901a201bb678d4
Hassan,Zakaria <z.hassan@student.fontys.nl>
2023-12-11 20:58:59 +0100

    deploying kubernetes

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -29,14 +29,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API V1");
     });
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
