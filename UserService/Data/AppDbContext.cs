1e7b2f18f37370febe0523e8394b54cef95c2dec
Hassan,Zakaria <z.hassan@student.fontys.nl>
2023-12-15 15:33:07 +0100

    X

--- UserService/Data/AppDbContext.cs
+++ UserService/Data/AppDbContext.cs
@@ -15,7 +15,7 @@ namespace UserService.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=user-service;Database=mypostgresdb;Username=mysecretuser;Password=mysecretpassword;");
+            optionsBuilder.UseNpgsql("Host=postgres-service;Database=mypostgresdb;Username=mysecretuser;Password=mysecretpassword;");
         }
 
 
