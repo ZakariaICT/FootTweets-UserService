e7d2dfa6d24ca76bdddb006a77b933cc9db1b6be
Hassan,Zakaria <z.hassan@student.fontys.nl>
2024-01-09 14:55:51 +0100

    x

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System;
-using System.Collections.Generic;
 using UserService.DTO;
 using UserService.Model;
 using UserService.Repositories;
@@ -19,14 +15,12 @@ namespace UserService.Controllers
         private IUserRepo _userRepo;
         private IMapper _mapper;
 
+
         public UserController(IConfiguration configuration, IUserRepo userRepo, IMapper mapper)
         {
             _configuration = configuration;
             _userRepo = userRepo;
             _mapper = mapper;
-
-            // Start listening for RabbitMQ messages
-            ListenForRabbitMQMessages();
         }
 
         [HttpGet("all")]
@@ -38,6 +32,7 @@ namespace UserService.Controllers
 
             return Ok(_mapper.Map<IEnumerable<UsersReadDTO>>(users));
         }
+
 
         [HttpGet("{id}", Name = "GetUserByID")]
         public ActionResult<UsersReadDTO> GetUserByID(Guid id)
@@ -63,76 +58,28 @@ namespace UserService.Controllers
             var userDTO = _mapper.Map<UsersReadDTO>(userModel);
 
             // Send RabbitMQ message
-            try
-            {
-                var factory = new ConnectionFactory
-                {
-                    Uri = new Uri(_configuration["RabbitMQConnection"])
-                };
+            //try
+            //{
+            //    var factory = new ConnectionFactory
+            //    {
+            //        Uri = new Uri(_configuration["RabbitMQConnection"])
+            //    };
 
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    var rabbitMQService = new RabbitMQService(channel);
-                    rabbitMQService.SendMessage($"New user created: {userDTO.Username}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Could not send RabbitMQ message: {ex.Message}");
-            }
+            //    using (var connection = factory.CreateConnection())
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        var rabbitMQService = new RabbitMQService(channel);
+            //        rabbitMQService.SendMessage($"New user created: {userDTO.Username}");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Could not send RabbitMQ message: {ex.Message}");
+            //}
 
             return CreatedAtRoute(nameof(GetUserByID), new { Id = userDTO.Id }, userDTO);
         }
 
-        private void ListenForRabbitMQMessages()
-        {
-            try
-            {
-                var factory = new ConnectionFactory
-                {
-                    Uri = new Uri(_configuration["RabbitMQConnection"])
-                };
 
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    // Declare the queue if it doesn't exist
-                    channel.QueueDeclare(queue: "user_creation_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-                    // Set up the consumer
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body.ToArray();
-                        var message = System.Text.Encoding.UTF8.GetString(body);
-
-                        // Process the message and create a user in the database
-                        CreateUserInDatabase(message);
-
-                        // Acknowledge the message
-                        channel.BasicAck(ea.DeliveryTag, false);
-                    };
-
-                    // Start consuming messages
-                    channel.BasicConsume(queue: "user_creation_queue", autoAck: false, consumer: consumer);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Could not set up RabbitMQ consumer: {ex.Message}");
-            }
-        }
-
-        private void CreateUserInDatabase(string message)
-        {
-            // Process the RabbitMQ message and create a user in the database
-            // Example: Deserialize JSON message or parse the message content
-            var user = new Users(); // Replace with your actual Users model
-            // Populate user properties from the message
-
-            _userRepo.CreateUser(user);
-            _userRepo.saveChanges();
-        }
     }
 }
--- UserService/RabbitMQService.cs
+++ UserService/RabbitMQService.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using System;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 public class RabbitMQService
 {
@@ -19,5 +20,28 @@ public class RabbitMQService
         _channel.BasicPublish(exchange: "", routingKey: "user_registration_queue", basicProperties: null, body: body);
 
         Console.WriteLine($" [x] Sent '{message}'");
+    }
+
+    public void ListenForMessages()
+    {
+        // Declare the queue if it doesn't exist
+        _channel.QueueDeclare(queue: "user_registration_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        // Set up the consumer
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            // Process the message (e.g., create a user in the database)
+            Console.WriteLine($" [x] Received '{message}'");
+
+            // Acknowledge the message
+            _channel.BasicAck(ea.DeliveryTag, false);
+        };
+
+        // Start consuming messages
+        _channel.BasicConsume(queue: "user_registration_queue", autoAck: false, consumer: consumer);
     }
 }
