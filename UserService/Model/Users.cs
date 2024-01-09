60a147feb8f5da885891bc75d9483d0172dc62ca
Hassan,Zakaria <z.hassan@student.fontys.nl>
2024-01-09 13:20:28 +0100

    Update Users.cs

--- UserService/Model/Users.cs
+++ UserService/Model/Users.cs
@@ -1,10 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 
 namespace UserService.Model
 {
-    [Table("users")]
+    
     public class Users
     {
         [Required]
